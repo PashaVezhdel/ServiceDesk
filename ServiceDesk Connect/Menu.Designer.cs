41b1a77f97c1464ce46b573d7069c478060ae80d
Pasha_Vezhdel <pashavezdel@gmail.com>
2026-02-08 01:54:08 +0200

    Final V1

--- ServiceDesk Connect/Menu.Designer.cs
+++ ServiceDesk Connect/Menu.Designer.cs
@@ -23,6 +23,7 @@
 
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu));
             this.pnlMain = new System.Windows.Forms.Panel();
             this.lblTitle = new System.Windows.Forms.Label();
             this.lblUser = new System.Windows.Forms.Label();
@@ -50,8 +51,9 @@
             this.pnlMain.Controls.Add(this.btnSend);
             this.pnlMain.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pnlMain.Location = new System.Drawing.Point(0, 0);
+            this.pnlMain.Name = "pnlMain";
             this.pnlMain.Padding = new System.Windows.Forms.Padding(25);
-            this.pnlMain.Size = new System.Drawing.Size(380, 420);
+            this.pnlMain.Size = new System.Drawing.Size(380, 500);
             this.pnlMain.TabIndex = 0;
 
             this.lblTitle.AutoSize = true;
@@ -66,7 +68,7 @@
             this.lblUser.AutoSize = true;
             this.lblUser.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.lblUser.ForeColor = System.Drawing.Color.Gray;
-            this.lblUser.Location = new System.Drawing.Point(22, 65);
+            this.lblUser.Location = new System.Drawing.Point(22, 70);
             this.lblUser.Name = "lblUser";
             this.lblUser.Size = new System.Drawing.Size(64, 15);
             this.lblUser.TabIndex = 1;
@@ -74,7 +76,7 @@
 
             this.txtUser.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtUser.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this.txtUser.Location = new System.Drawing.Point(25, 83);
+            this.txtUser.Location = new System.Drawing.Point(25, 90);
             this.txtUser.Name = "txtUser";
             this.txtUser.Size = new System.Drawing.Size(330, 25);
             this.txtUser.TabIndex = 2;
@@ -82,7 +84,7 @@
             this.lblHardware.AutoSize = true;
             this.lblHardware.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.lblHardware.ForeColor = System.Drawing.Color.Gray;
-            this.lblHardware.Location = new System.Drawing.Point(22, 125);
+            this.lblHardware.Location = new System.Drawing.Point(22, 135);
             this.lblHardware.Name = "lblHardware";
             this.lblHardware.Size = new System.Drawing.Size(134, 15);
             this.lblHardware.TabIndex = 3;
@@ -91,7 +93,7 @@
             this.cmbHardware.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbHardware.Font = new System.Drawing.Font("Segoe UI", 10F);
             this.cmbHardware.FormattingEnabled = true;
-            this.cmbHardware.Location = new System.Drawing.Point(25, 143);
+            this.cmbHardware.Location = new System.Drawing.Point(25, 155);
             this.cmbHardware.Name = "cmbHardware";
             this.cmbHardware.Size = new System.Drawing.Size(330, 25);
             this.cmbHardware.TabIndex = 4;
@@ -99,7 +101,7 @@
             this.lblPriority.AutoSize = true;
             this.lblPriority.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.lblPriority.ForeColor = System.Drawing.Color.Gray;
-            this.lblPriority.Location = new System.Drawing.Point(22, 185);
+            this.lblPriority.Location = new System.Drawing.Point(22, 200);
             this.lblPriority.Name = "lblPriority";
             this.lblPriority.Size = new System.Drawing.Size(81, 15);
             this.lblPriority.TabIndex = 5;
@@ -108,7 +110,7 @@
             this.cmbPriority.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbPriority.Font = new System.Drawing.Font("Segoe UI", 10F);
             this.cmbPriority.FormattingEnabled = true;
-            this.cmbPriority.Location = new System.Drawing.Point(25, 203);
+            this.cmbPriority.Location = new System.Drawing.Point(25, 220);
             this.cmbPriority.Name = "cmbPriority";
             this.cmbPriority.Size = new System.Drawing.Size(330, 25);
             this.cmbPriority.TabIndex = 6;
@@ -116,7 +118,7 @@
             this.lblIssue.AutoSize = true;
             this.lblIssue.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.lblIssue.ForeColor = System.Drawing.Color.Gray;
-            this.lblIssue.Location = new System.Drawing.Point(22, 245);
+            this.lblIssue.Location = new System.Drawing.Point(22, 265);
             this.lblIssue.Name = "lblIssue";
             this.lblIssue.Size = new System.Drawing.Size(99, 15);
             this.lblIssue.TabIndex = 7;
@@ -124,7 +126,7 @@
 
             this.txtIssue.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtIssue.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this.txtIssue.Location = new System.Drawing.Point(25, 263);
+            this.txtIssue.Location = new System.Drawing.Point(25, 285);
             this.txtIssue.Multiline = true;
             this.txtIssue.Name = "txtIssue";
             this.txtIssue.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
@@ -136,7 +138,7 @@
             this.btnSend.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnSend.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
             this.btnSend.ForeColor = System.Drawing.Color.White;
-            this.btnSend.Location = new System.Drawing.Point(25, 360);
+            this.btnSend.Location = new System.Drawing.Point(25, 390);
             this.btnSend.Name = "btnSend";
             this.btnSend.Size = new System.Drawing.Size(330, 40);
             this.btnSend.TabIndex = 9;
@@ -146,9 +148,10 @@
 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(380, 445);
+            this.ClientSize = new System.Drawing.Size(380, 480);
             this.Controls.Add(this.pnlMain);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Menu";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
--- ServiceDesk Connect/authorization.Designer.cs
+++ ServiceDesk Connect/authorization.Designer.cs
@@ -20,114 +20,142 @@
 
         private void InitializeComponent()
         {
-            this.pnlMain = new System.Windows.Forms.Panel();
-            this.lblTitle = new System.Windows.Forms.Label();
-            this.lblLogin = new System.Windows.Forms.Label();
-            this.txtLogin = new System.Windows.Forms.TextBox();
-            this.lblPassword = new System.Windows.Forms.Label();
-            this.txtPassword = new System.Windows.Forms.TextBox();
-            this.chkRemember = new System.Windows.Forms.CheckBox();
-            this.btnLogin = new System.Windows.Forms.Button();
-            this.pnlMain.SuspendLayout();
-            this.SuspendLayout();
-
-            this.pnlMain.Controls.Add(this.lblTitle);
-            this.pnlMain.Controls.Add(this.lblLogin);
-            this.pnlMain.Controls.Add(this.txtLogin);
-            this.pnlMain.Controls.Add(this.lblPassword);
-            this.pnlMain.Controls.Add(this.txtPassword);
-            this.pnlMain.Controls.Add(this.chkRemember);
-            this.pnlMain.Controls.Add(this.btnLogin);
-            this.pnlMain.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pnlMain.Location = new System.Drawing.Point(0, 0);
-            this.pnlMain.Size = new System.Drawing.Size(350, 400);
-            this.pnlMain.TabIndex = 0;
-
-            this.lblTitle.AutoSize = true;
-            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblTitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
-            this.lblTitle.Location = new System.Drawing.Point(42, 30); 
-            this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(201, 37);
-            this.lblTitle.TabIndex = 0;
-            this.lblTitle.Text = "Вхід у систему";
-
-            this.lblLogin.AutoSize = true;
-            this.lblLogin.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblLogin.ForeColor = System.Drawing.Color.Gray;
-            this.lblLogin.Location = new System.Drawing.Point(45, 90);
-            this.lblLogin.Name = "lblLogin";
-            this.lblLogin.Size = new System.Drawing.Size(79, 19);
-            this.lblLogin.TabIndex = 1;
-            this.lblLogin.Text = "Telegram ID";
-
-            this.txtLogin.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txtLogin.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.txtLogin.Location = new System.Drawing.Point(45, 115);
-            this.txtLogin.Name = "txtLogin";
-            this.txtLogin.PlaceholderText = "Введіть ID (дізнатись: @userinfobot)";
-            this.txtLogin.Size = new System.Drawing.Size(260, 27);
-            this.txtLogin.TabIndex = 2;
-
-            this.lblPassword.AutoSize = true;
-            this.lblPassword.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblPassword.ForeColor = System.Drawing.Color.Gray;
-            this.lblPassword.Location = new System.Drawing.Point(45, 160);
-            this.lblPassword.Name = "lblPassword";
-            this.lblPassword.Size = new System.Drawing.Size(56, 19);
-            this.lblPassword.TabIndex = 3;
-            this.lblPassword.Text = "Пароль";
-
-            this.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txtPassword.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.txtPassword.Location = new System.Drawing.Point(45, 185);
-            this.txtPassword.Name = "txtPassword";
-            this.txtPassword.PlaceholderText = ""; // ПРИБРАВ КРАПКИ
-            this.txtPassword.Size = new System.Drawing.Size(260, 27);
-            this.txtPassword.TabIndex = 4;
-            this.txtPassword.UseSystemPasswordChar = true;
-
-
-            this.chkRemember.AutoSize = true;
-            this.chkRemember.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.chkRemember.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.chkRemember.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
-            this.chkRemember.Location = new System.Drawing.Point(45, 230);
-            this.chkRemember.Name = "chkRemember";
-            this.chkRemember.Size = new System.Drawing.Size(134, 23);
-            this.chkRemember.TabIndex = 5;
-            this.chkRemember.Text = "Запам\'ятати мене";
-            this.chkRemember.UseVisualStyleBackColor = true;
-
-            this.btnLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(120)))), ((int)(((byte)(215)))));
-            this.btnLogin.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnLogin.FlatAppearance.BorderSize = 0;
-            this.btnLogin.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(100)))), ((int)(((byte)(190)))));
-            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnLogin.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.btnLogin.ForeColor = System.Drawing.Color.White;
-            this.btnLogin.Location = new System.Drawing.Point(45, 280);
-            this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(260, 45);
-            this.btnLogin.TabIndex = 6;
-            this.btnLogin.Text = "Увійти";
-            this.btnLogin.UseVisualStyleBackColor = false;
-            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
-
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(350, 400); 
-            this.Controls.Add(this.pnlMain);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.MaximizeBox = false;
-            this.Name = "authorization";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Авторизація";
-            this.Load += new System.EventHandler(this.authorization_Load);
-            this.pnlMain.ResumeLayout(false);
-            this.pnlMain.PerformLayout();
-            this.ResumeLayout(false);
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(authorization));
+            pnlMain = new Panel();
+            lblTitle = new Label();
+            lblLogin = new Label();
+            txtLogin = new TextBox();
+            lblPassword = new Label();
+            txtPassword = new TextBox();
+            chkRemember = new CheckBox();
+            btnLogin = new Button();
+            pnlMain.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pnlMain
+            // 
+            pnlMain.Controls.Add(lblTitle);
+            pnlMain.Controls.Add(lblLogin);
+            pnlMain.Controls.Add(txtLogin);
+            pnlMain.Controls.Add(lblPassword);
+            pnlMain.Controls.Add(txtPassword);
+            pnlMain.Controls.Add(chkRemember);
+            pnlMain.Controls.Add(btnLogin);
+            pnlMain.Dock = DockStyle.Fill;
+            pnlMain.Location = new Point(0, 0);
+            pnlMain.Margin = new Padding(4, 5, 4, 5);
+            pnlMain.Name = "pnlMain";
+            pnlMain.Size = new Size(500, 667);
+            pnlMain.TabIndex = 0;
+            // 
+            // lblTitle
+            // 
+            lblTitle.AutoSize = true;
+            lblTitle.Font = new Font("Segoe UI", 20F);
+            lblTitle.ForeColor = Color.FromArgb(30, 30, 30);
+            lblTitle.Location = new Point(60, 50);
+            lblTitle.Margin = new Padding(4, 0, 4, 0);
+            lblTitle.Name = "lblTitle";
+            lblTitle.Size = new Size(280, 54);
+            lblTitle.TabIndex = 0;
+            lblTitle.Text = "Вхід у систему";
+            // 
+            // lblLogin
+            // 
+            lblLogin.AutoSize = true;
+            lblLogin.Font = new Font("Segoe UI", 10F);
+            lblLogin.ForeColor = Color.Gray;
+            lblLogin.Location = new Point(64, 150);
+            lblLogin.Margin = new Padding(4, 0, 4, 0);
+            lblLogin.Name = "lblLogin";
+            lblLogin.Size = new Size(115, 28);
+            lblLogin.TabIndex = 1;
+            lblLogin.Text = "Telegram ID";
+            // 
+            // txtLogin
+            // 
+            txtLogin.BorderStyle = BorderStyle.FixedSingle;
+            txtLogin.Font = new Font("Segoe UI", 11F);
+            txtLogin.Location = new Point(64, 192);
+            txtLogin.Margin = new Padding(4, 5, 4, 5);
+            txtLogin.Name = "txtLogin";
+            txtLogin.PlaceholderText = "Введіть ID (дізнатись: @userinfobot)";
+            txtLogin.Size = new Size(371, 37);
+            txtLogin.TabIndex = 2;
+            // 
+            // lblPassword
+            // 
+            lblPassword.AutoSize = true;
+            lblPassword.Font = new Font("Segoe UI", 10F);
+            lblPassword.ForeColor = Color.Gray;
+            lblPassword.Location = new Point(64, 267);
+            lblPassword.Margin = new Padding(4, 0, 4, 0);
+            lblPassword.Name = "lblPassword";
+            lblPassword.Size = new Size(81, 28);
+            lblPassword.TabIndex = 3;
+            lblPassword.Text = "Пароль";
+            // 
+            // txtPassword
+            // 
+            txtPassword.BorderStyle = BorderStyle.FixedSingle;
+            txtPassword.Font = new Font("Segoe UI", 11F);
+            txtPassword.Location = new Point(64, 308);
+            txtPassword.Margin = new Padding(4, 5, 4, 5);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(371, 37);
+            txtPassword.TabIndex = 4;
+            txtPassword.UseSystemPasswordChar = true;
+            // 
+            // chkRemember
+            // 
+            chkRemember.AutoSize = true;
+            chkRemember.FlatStyle = FlatStyle.Flat;
+            chkRemember.Font = new Font("Segoe UI", 10F);
+            chkRemember.ForeColor = Color.FromArgb(60, 60, 60);
+            chkRemember.Location = new Point(64, 383);
+            chkRemember.Margin = new Padding(4, 5, 4, 5);
+            chkRemember.Name = "chkRemember";
+            chkRemember.Size = new Size(194, 32);
+            chkRemember.TabIndex = 5;
+            chkRemember.Text = "Запам'ятати мене";
+            chkRemember.UseVisualStyleBackColor = true;
+            // 
+            // btnLogin
+            // 
+            btnLogin.BackColor = Color.FromArgb(0, 120, 215);
+            btnLogin.Cursor = Cursors.Hand;
+            btnLogin.FlatAppearance.BorderSize = 0;
+            btnLogin.FlatAppearance.MouseOverBackColor = Color.FromArgb(0, 100, 190);
+            btnLogin.FlatStyle = FlatStyle.Flat;
+            btnLogin.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
+            btnLogin.ForeColor = Color.White;
+            btnLogin.Location = new Point(64, 467);
+            btnLogin.Margin = new Padding(4, 5, 4, 5);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(371, 75);
+            btnLogin.TabIndex = 6;
+            btnLogin.Text = "Увійти";
+            btnLogin.UseVisualStyleBackColor = false;
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // authorization
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(500, 667);
+            Controls.Add(pnlMain);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(4, 5, 4, 5);
+            MaximizeBox = false;
+            Name = "authorization";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Авторизація";
+            Load += authorization_Load;
+            pnlMain.ResumeLayout(false);
+            pnlMain.PerformLayout();
+            ResumeLayout(false);
         }
     }
 }
